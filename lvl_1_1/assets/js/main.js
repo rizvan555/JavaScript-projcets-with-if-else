00540b5b0fbbde022e66ddb4b0d2ed12c6551de8
Rizvan555 <rizvan84@gmx.de>
2023-02-22 09:18:03 +0100

    changed

--- lvl_1_1/assets/js/main.js
+++ lvl_1_1/assets/js/main.js
@@ -3,8 +3,10 @@ const result = document.querySelector("p");
 
 function adult() {
   if (checkOld.value >= 18) {
+    console.log(checkOld.value >= 18);
     result.innerHTML = "volljährig";
-  } else (checkOld.value <= 18) {
+  } else {
+    console.log(checkOld.value >= 18);
     result.innerHTML = "mindestjährig";
   }
 }
